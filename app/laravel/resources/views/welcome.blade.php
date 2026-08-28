8aa604f5dfbcb7dc1077a82c1099f29b8a87acb0
ynakamitsu-ship-it <y.nakamitsu@a-cial.com>
2026-08-28 19:15:24 +0900

    20260828-ユーザー認証（ログイン・ログアウト）-完了

--- app/laravel/resources/views/welcome.blade.php
+++ app/laravel/resources/views/welcome.blade.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -16,6 +18,9 @@
         </style>
     </head>
     <body class="antialiased">
+        <button class="btn btn-primary">
+    テストボタン
+</button>
         <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
             @if (Route::has('login'))
                 <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
--- app/laravel/vite.config.js
+++ app/laravel/vite.config.js
@@ -2,9 +2,22 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        strictPort: true,
+        hmr: {
+            host: 'localhost',
+            port: 5173,
+        },
+    },
+
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/sass/app.scss',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
     ],
